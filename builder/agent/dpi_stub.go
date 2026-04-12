2b0a74caf57cae7c4ab4840865e77a24f68470ba
Sanblch <logovopost@yandex.ru>
2026-04-12 13:30:58 +0300

    rmm без аделенного рабочего стола, он в отдельном проекте

--- builder/agent/dpi_stub.go
+++
@@ -1,7 +0,0 @@
-//go:build !windows
-// builder/agent/dpi_stub.go
-package main
-
-// Заглушки для других ОС
-func initWindowsDPI()                   {}
-func getPhysicalScreenSize() (int, int) { return 0, 0 }
